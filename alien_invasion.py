c20ad36c5f885491f12750bcdd361d5d9d20aefb
T-Fincher-04 <tfincher1@student.cscc.edu>
2025-07-14 15:50:14 -0400

    P2 - Created the alien, displayed on screen, no movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from arsenal import Arsenal
+from alien import Alien
 
 class AlienInvasion:
 
@@ -28,6 +29,7 @@ class AlienInvasion:
         self.laser_sound.set_volume(0.7)
 
         self.ship = Ship(self, Arsenal(self))
+        self.alien = Alien(self, 10,10)
         self.ship.rect.midleft = (0, self.settings.screen_h // 2)
         self.ship.angle = 90
 
@@ -36,12 +38,14 @@ class AlienInvasion:
         while self.running:
             self._check_events()
             self.ship.update()
+            self.alien.update()
             self._update_screen()
             self.clock.tick(self.settings.FPS)
 
     def _update_screen(self):
         self.screen.blit(self.bg, (0,0))
         self.ship.draw()
+        self.alien.draw_alien()
         pygame.display.flip()
 
     def _check_events(self):
