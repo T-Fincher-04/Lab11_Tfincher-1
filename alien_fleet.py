a57dfa1f6b14a44340010f621c09d35670d7db2d
T-Fincher-04 <tfincher1@student.cscc.edu>
2025-07-14 19:46:18 -0400

    p2 - Horizontal spacing added to fleet positioning

--- alien_fleet.py
+++ alien_fleet.py
@@ -28,7 +28,8 @@ class AlienFleet:
 
         for col in range(fleet_w):
             current_x = alien_w * col + x_offset
-            self.create_alien(current_x, 10)
+            if col % 2 == 0:
+             self.create_alien(current_x, 10)
     
     def calculate_fleet_size(self, alien_w, screen_w):
         fleet_w = (screen_w//alien_w)
